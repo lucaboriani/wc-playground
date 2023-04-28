15129a4462bea91621ee7dcc5e224c67fe52b766
zp <zeropara@gmail.com>
2023-04-28 12:09:19 +0200

    auth with github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,27 @@ import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
 import react from '@astrojs/react';
 import vue from "@astrojs/vue";
-
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-//import node from "@astrojs/node";
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://wc-lit-playground.netlify.app/',
-  integrations: [
-    lit(), 
-    react(), 
-    vue({
-      template: {
-        compilerOptions: {
-          // treat all tags with a dash as custom elements
-          isCustomElement: tag => tag.includes('-')
-        }
+  site: 'https://wc-lit-playground.netlify.app/',
+  integrations: [lit(), react(), vue({
+    template: {
+      compilerOptions: {
+        // treat all tags with a dash as custom elements
+        isCustomElement: tag => tag.includes('-')
       }
-    }),
-    tailwind(), 
-    svelte()
-  ],
+    }
+  }), tailwind(), svelte(), auth()],
   output: "server",
   adapter: netlify()
 });
